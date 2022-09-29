187fed84dc0e312f5bf673c9b1bf3342048a5880
xiaoxigua-1 <xiaoxigua-1@users.noreply.github.com>
2022-09-29 08:33:58 +0000

    deploy: 5764dc6bf8c74e83428437bde782fc0675311a64

---
+++ _next/static/chunks/pages/homework2-763daaf1875ac679.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[907],{1839:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/homework2",function(){return t(5920)}])},5920:function(n,u,t){"use strict";t.r(u);var _=t(5893),e=function(){return(0,_.jsx)("div",{})};u.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=1839)}),_N_E=n.O()}]);
