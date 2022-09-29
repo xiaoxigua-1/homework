eb5f88fb73a3e191c7e60947aa066b39af58c91c
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-09-29 16:12:46 +0800

    add gh-page action

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return defaultPathMap
+  },
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
