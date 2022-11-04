073de78baf53c75fe462514ef7196326752db9c6
xiao xigua <aijdfajodwsdf@gmail.com>
2022-11-04 02:04:48 +0000

    send line message add sticker

--- backend/homework/src/main.rs
+++ backend/homework/src/main.rs
@@ -7,7 +7,7 @@ use rocket::http::Status;
 use rocket::serde::Deserialize;
 use reqwest::{multipart, Client};
 
-#[derive(FromForm)]
+#[derive(FromForm, Clone)]
 struct FormPost {
     name: String,
     birthday: String,
@@ -16,6 +16,15 @@ struct FormPost {
     mobile: String,
     phone: String,
     message: String,
+    sticker: Option<Sticker>
+}
+
+#[derive(FromForm, Clone)]
+struct Sticker {
+    #[field(name = "packageId")]
+    package_id: String,
+    #[field(name = "stickerId")]
+    sticker_id: String,
 }
 
 #[derive(Deserialize)]
@@ -40,21 +49,25 @@ async fn line_send_message(form: Form<FormPost>, token: &String) { // Form<FromP
     let multipart = multipart::Form::new()
         .text("message", format!(
             "name: {}\nbirthday: {}\naddres: {}\nheight: {}\nmobile: {}\nphone: {}\nmessage: {}",
-            form.name,
-            form.birthday,
-            form.addres,
-            form.height,
-            form.mobile,
-            form.phone,
-            form.message,
+            &form.name,
+            &form.birthday,
+            &form.addres,
+            &form.height,
+            &form.mobile,
+            &form.phone,
+            &form.message,
         ));
+    let multipart = if let Some(sticker) = form.sticker.clone() {
+        multipart
+            .text("stickerId", sticker.sticker_id.clone())
+            .text("stickerPackageId", sticker.package_id.clone())
+    } else { multipart };
     let res = client.post("https://notify-api.line.me/api/notify") 
         .multipart(multipart)
         .header("Authorization", format!("Bearer {}", token))
         .send()
         .await
         .unwrap();
-
     println!("{}", res.status());
 }
 
